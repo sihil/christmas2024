00753d93bd00a9b55ac2cb5fd289ba8b672c0abb
Simon Hildrew <simon.hildrew@evenlydistributed.xyz>
2024-10-20 21:30:02 +0100

    Almost good fill

--- snowflake-card/sketch_snowflake_card.py
+++ snowflake-card/sketch_snowflake_card.py
@@ -26,23 +26,31 @@ class PolygonStoreEntry:
     name: str
 
 class PolygonGroup:
-    def __init__(self):
+    def __init__(self, name: str):
         self.polygons = []
         self.groups = {}
+        self.name = name
 
     def add_polygon(self, polygon: Polygon | MultiPolygon, layer: int, name: str):
         self.polygons.append(PolygonStoreEntry(layer, polygon, name))
 
-    def add_group(self, group: PolygonGroup, name: str):
+    def add_group(self, group: PolygonGroup, name: str, change_layer: int | None = None):
         if name in self.groups:
             raise ValueError(f"Group {name} already exists")
         self.groups[name] = group
 
     def all_geometries(self):
-        return self.polygons + [entry
+        return self.polygons + [PolygonStoreEntry(entry.layer, entry.geometry, f"{self.name}/{entry.name}")
                                 for group in self.groups.values()
                                 for entry in group.all_geometries()]
 
+    def draw(self, vsk: Vsketch):
+        for entry in self.all_geometries():
+            try:
+                vsk.stroke(entry.layer)
+                vsk.geometry(entry.geometry)
+            except ValueError as e:
+                raise ValueError(f"Error drawing {entry.name}") from e
 
 class SnowflakeCardSketch(vsketch.SketchClass):
     # Sketch parameters
@@ -56,10 +64,16 @@ class SnowflakeCardSketch(vsketch.SketchClass):
 
         # create the snowflake
         vsk.penWidth("0.7mm")
+
+        sketch_group = PolygonGroup("snowflake card")
+
         base_star = self.filled_hexagon_star2(13, 9, self.size / 10, 1, 0.07)
-        vsk.sketch(base_star)
+        sketch_group.add_group(base_star, "star1")
+
         sector_star = self.filled_hexagon_star_with_sector_ends(7, 5, self.size / 15, 1, sector_offset=1.2, sector_width=0.7, pen_width=0.07)
-        vsk.geometry(sector_star)
+        sketch_group.add_group(sector_star, "star2")
+
+        sketch_group.draw(vsk)
 
     def hexagon(self, x: float, y: float, radius: float) -> BaseGeometry:
         points = []
@@ -68,22 +82,22 @@ class SnowflakeCardSketch(vsketch.SketchClass):
             points.append((x + radius * math.cos(angle), y + radius * math.sin(angle)))
         return Polygon(points)
 
-    def filled_hexagon_star_with_sector_ends(self, x: float, y: float, radius: float, thickness: float, sector_offset: float, sector_width: float, pen_width: float) -> MultiPolygon:
+    def filled_hexagon_star_with_sector_ends(self, x: float, y: float, radius: float, thickness: float, sector_offset: float, sector_width: float, pen_width: float) -> PolygonGroup:
+        group = PolygonGroup("filled_hexagon_star_with_sector_ends")
         star = self.hexagon_star(x, y, radius, thickness)
         sector_ends = []
         for i in range(6):
             sector = self.elongated_hexagon(x + sector_offset, y, radius - sector_offset, thickness + sector_width)
-#            filled_sector = self.filled_polygon(sector, -pen_width)
-            sector_ends.append(affinity.rotate(geom=sector, origin=(x, y), angle=i*60))
+#           filled_sector = self.filled_polygon(sector, -pen_width)
+            rotated_sector = affinity.rotate(geom=sector, origin=(x, y), angle=i*60)
+            sector_ends.append(rotated_sector)
+            group.add_group(self.filled_polygon(rotated_sector, -pen_width), f"sector_{i}")
 
-        star = difference(star, unary_union(sector_ends))
-        filled_sector_ends = [self.filled_polygon(sector, -pen_width) for sector in sector_ends]
-        all_multi_polygons = [star, *filled_sector_ends]
-        return MultiPolygon([geom
-                             for multi_polygon in all_multi_polygons
-                             for geom in multi_polygon.geoms
-                             if not geom.is_empty])
-        # return MultiPolygon(all_multi_polygons)
+        star_centre = difference(star, unary_union(sector_ends))
+        geoms = list(star_centre.geoms)
+        group.add_group(self.filled_polygon(geoms[0], pen_width), "star_centre")
+
+        return group
 
     def hexagon_star_with_sector_ends(self, x: float, y: float, radius: float, thickness: float, sector_offset: float, sector_width: float) -> BaseGeometry:
         star = self.hexagon_star(x, y, radius, thickness)
@@ -103,9 +117,10 @@ class SnowflakeCardSketch(vsketch.SketchClass):
             polygons.append(self.hexagon_star(x, y, radius+thickness_offset*math.tan(math.radians(30)), thickness+thickness_offset))
         return MultiPolygon(polygons)
 
-    def filled_polygon(self, polygon: Polygon, pen_width: float) -> MultiPolygon:
+    def filled_polygon(self, polygon: Polygon, pen_width: float) -> PolygonGroup:
+        group = PolygonGroup("filled_polygon")
         polygon_ring: LinearRing = polygon.exterior
-        polygons = [polygon]
+        group.add_polygon(polygon, 1, "outer_polygon")
         thickness_offset = 0
         breakout = 50
         while True:
@@ -114,16 +129,16 @@ class SnowflakeCardSketch(vsketch.SketchClass):
             offset = polygon_ring.offset_curve(thickness_offset)
             if offset.is_empty:
                 break
-            polygons.append(Polygon(offset))
+            group.add_polygon(Polygon(offset), 1, f"fill_polygon_{thickness_offset}")
             breakout -= 1
             if breakout < 0:
                 raise ValueError("Too many iterations")
-        return MultiPolygon(polygons)
+        return group
 
-    def filled_hexagon_star2(self, x: float, y: float, radius: float, thickness: float, pen_width: float) -> Vsketch:
-        hexagon_star = Vsketch()
+    def filled_hexagon_star2(self, x: float, y: float, radius: float, thickness: float, pen_width: float) -> PolygonGroup:
+        hexagon_star = PolygonGroup("filled_hexagon_star2")
         outer_hexagon_star = self.hexagon_star(x, y, radius, thickness)
-        hexagon_star.geometry(outer_hexagon_star)
+        hexagon_star.add_polygon(outer_hexagon_star, 1, "outer_hexagon_star")
         ring: LinearRing = outer_hexagon_star.exterior
         print(f"Creating filled hexagon star at {x}, {y} with radius {radius}, thickness {thickness} and pen_width {pen_width}")
 
@@ -132,7 +147,7 @@ class SnowflakeCardSketch(vsketch.SketchClass):
             thickness_offset -= pen_width
             print(f"Thickness offset: {thickness_offset}")
             offset = ring.offset_curve(thickness_offset)
-            hexagon_star.geometry(offset)
+            hexagon_star.add_polygon(offset, 1, f"outer_hexagon_star_{thickness_offset}")
         return hexagon_star
 
     def hexagon_star(self, x: float, y: float, radius: float, thickness: float) -> Polygon:
